bb230d1d147a5cbbad60392b27d0bbe24564545a
xynydev <60004820+xynydev@users.noreply.github.com>
2024-03-28 15:56:44 +0200

    chore: add some scaffolding for the repo, shadcn and tailwind too

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import "./styles.css";
+import "./app.pcss";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+  target: document.getElementById("app") as HTMLElement,
 });
 
 export default app;
--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,10 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess({})],
+  kit: {
+    alias: {
+      "@/*": "./src/lib/*",
+    },
+  },
 };
