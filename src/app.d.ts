c2de580794c050096deb01a52fc6d842f8834170
xynydev <60004820+xynydev@users.noreply.github.com>
2024-04-17 21:51:08 +0300

    fix: cloudflare env vars boilerpate

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,15 @@ declare global {
         // interface Locals {}
         // interface PageData {}
         // interface PageState {}
-        // interface Platform {}
+        interface Platform {
+            env: {
+                COUNTER: DurableObjectNamespace;
+            };
+            context: {
+                waitUntil(promise: Promise<any>): void;
+            };
+            caches: CacheStorage & { default: Cache };
+        }
     }
 }
 
