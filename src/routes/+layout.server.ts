97b33c33e5aa4fe7c3b7959b531037d39c7d08a8
xynydev <60004820+xynydev@users.noreply.github.com>
2024-04-20 12:33:34 +0300

    chore: send cookies to client for debugging

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
-export async function load({ locals }) {
+export async function load({ locals, cookies }) {
     return {
-        githubUser: locals.githubUser
+        githubUser: locals.githubUser,
+        cookies: JSON.stringify(cookies)
     };
 }
