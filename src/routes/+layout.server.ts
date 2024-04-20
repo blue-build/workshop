ec2b5eb8de3c3bd7d48836c59a54b92f777542fb
xynydev <60004820+xynydev@users.noreply.github.com>
2024-04-20 12:52:18 +0300

    chore: test doing auth in server layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,12 @@
-export async function load({ locals }) {
+export async function load({ locals, cookies }) {
+    const githubToken = cookies.get("github_oauth_token") ?? null;
+    const githubUserResponse = await fetch("https://api.github.com/user", {
+        headers: {
+            Authorization: `Bearer ${githubToken}`
+        }
+    });
+
     return {
-        githubUser: locals.githubUser
+        githubUser: githubUserResponse.ok ? await githubUserResponse.json() : null;
     };
 }
